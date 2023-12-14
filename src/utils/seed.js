b94d4125ddca01a410d4d5bc8884d5d554f326e5
zach <zholmes430@gmail.com>
2023-12-14 20:40:13 +1000

    Refactor seed.js file

--- src/utils/seed.js
+++ src/utils/seed.js
@@ -1,27 +1,29 @@
-const { databaseConnect, databaseDisconnect } = require('../database');
-const User = require('../models/UserModel');
-const Ticket = require('../models/TicketModel');
-const Comment = require('../models/CommentModel');
+const { databaseConnect, databaseDisconnect } = require("../database");
+const User = require("../models/UserModel");
+const Ticket = require("../models/TicketModel");
+const Comment = require("../models/CommentModel");
 // import seed data
-const { usersSeedList, ticketsSeedList, commentsSeedList } = require('./seedData');
+const {
+  usersSeedList,
+  ticketsSeedList,
+  commentsSeedList,
+} = require("./seedData");
 
-require('dotenv').config();
-
+require("dotenv").config();
 
 databaseConnect()
-    .then(async () => {
-        console.log(`wiping database...`)
-        await User.deleteMany({});
-        await Ticket.deleteMany({});
-        await Comment.deleteMany({});
-        
-        console.log('Seeding database...');
-        await User.insertMany(usersSeedList);
-        await Ticket.insertMany(ticketsSeedList);
-        await Comment.insertMany(commentsSeedList);
+  .then(async () => {
+    console.log(`Wiping database...`);
+    await User.deleteMany({});
+    await Ticket.deleteMany({});
+    await Comment.deleteMany({});
 
-        console.log('Database seeded!');
-    })
-    .catch(console.error)
-    .finally(databaseDisconnect);
+    console.log("Seeding database...");
+    await User.insertMany(usersSeedList);
+    await Ticket.insertMany(ticketsSeedList);
+    await Comment.insertMany(commentsSeedList);
 
+    console.log("Database seeded!");
+  })
+  .catch(console.error)
+  .finally(databaseDisconnect);
