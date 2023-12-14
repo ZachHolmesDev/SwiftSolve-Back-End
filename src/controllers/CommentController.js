8d5677ae0061bc5930ac637a2c6eccd253e29933
zach <zholmes430@gmail.com>
2023-12-14 19:50:09 +1000

    Refactor seed.js

--- src/controllers/CommentController.js
+++ src/controllers/CommentController.js
@@ -1,10 +1,10 @@
 const CommentModel = require('../models/CommentModel'); 
-const { param } = require('../routers/UserRouter');
+// const { param } = require('../routers/UserRouter');
 
 const CommentController = {
 
     // Route: /ticket/:ticketId/comment
-    // Logic to get all comments for a specific ticket
+    // get all comments for a specific ticket
     async getCommentsForTicket(request, response) {
         response.json({
             message: "Test getting all comments for a specific ticket tick ID = " + request.params.ticketId
@@ -12,7 +12,7 @@ const CommentController = {
     },
 
     // Route: /ticket/:ticketId/comment/:commentId
-    // Logic to get a specific comment by ID
+    // get a specific comment by ID
     async getCommentById(request, response) {
         response.json({
             message: "Test getting a specific comment by ID : " + request.params.commentId
@@ -20,19 +20,19 @@ const CommentController = {
     },
     
     // Route: /ticket/:ticketId/comment
-    // Logic to add a comment to a ticket
+    // add a comment to a ticket
     async createComment(request, response) {
 
     },
 
     // Route: /ticket/:ticketId/comment/:commentId
-    // Logic to update a comment
+    // update a comment
     async updateComment(request, response) {
 
     },
 
     // Route: /ticket/:ticketId/comment/:commentId
-    // Logic to delete a comment
+    // delete a comment
     async deleteComment(request, response) {
 
     },
--- src/utils/seed.js
+++ src/utils/seed.js
@@ -1,41 +1,27 @@
-const mongoose = require('mongoose');
 const { databaseConnect, databaseDisconnect } = require('../database');
-const { User } = require('../models/user');
-const { Ticket } = require('../models/ticket');
-const { Comment } = require('../models/comment');
-const { usersSeedList, ticketsSeedList, commentsSeedList } = require('./seedData'); // Import your seed data
-
+const User = require('../models/UserModel');
+const Ticket = require('../models/TicketModel');
+const Comment = require('../models/CommentModel');
+// import seed data
+const { usersSeedList, ticketsSeedList, commentsSeedList } = require('./seedData');
 
 require('dotenv').config();
 
-databaseConnect().then(async () => {
-    console.log('Seeding database...');
-    // Delete all users tickets and comments
-    await User.deleteMany({});
-    await Ticket.deleteMany({});
-    await Comment.deleteMany({});
-    
-    // insert users 
-    const users = await User.insertMany(usersSeedList);
 
-    // Replace placeholder 'userObjectId' with actual user _id in ticketsSeedList
-    // ticketsSeedList.forEach(ticket => {
-    //     ticket.createdBy = users[0]._id;
-    //     ticket.assignedTo = users[0]._id;
-    // }); 
+databaseConnect()
+    .then(async () => {
+        console.log(`wiping database...`)
+        await User.deleteMany({});
+        await Ticket.deleteMany({});
+        await Comment.deleteMany({});
+        
+        console.log('Seeding database...');
+        await User.insertMany(usersSeedList);
+        await Ticket.insertMany(ticketsSeedList);
+        await Comment.insertMany(commentsSeedList);
 
-    // Create and save tickets
-    const tickets = await Ticket.insertMany(ticketsSeedList);
-
-    // Replace placeholder 'userObjectId' and 'ticketObjectId' with actual user and ticket _id in commentsSeedList
-    // commentsSeedList.forEach(comment => {
-    //     comment.createdById = users[0]._id;
-    //     comment.ticketId = tickets[0]._id;
-    // });
-
-    // Create and save comments
-    await Comment.insertMany(commentsSeedList);
-
-    console.log('Database seeded!');
-}).catch(console.error).finally(databaseDisconnect);
+        console.log('Database seeded!');
+    })
+    .catch(console.error)
+    .finally(databaseDisconnect);
 
